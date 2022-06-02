c5c412ca90b94e43f0382c61066af37ae470c2b9
jodev08 <j.stonskadev@gmail.com>
2022-06-02 11:41:56 +0200

    utilisateur connecter

--- src/App.js
+++ src/App.js
@@ -1,15 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 import { Routes, Route } from "react-router-dom";
 import ConnectModal from "./components/ConnectModal";
 import Accueil from "./pages/Accueil";
 import Login from "./components/Login";
 import SignUp from "./components/SignUp";
+import { onAuthStateChanged } from "firebase/auth";
+import { auth } from "./utils/firebase.config";
+import CreatePost from "./components/CreatePost";
 
 const App = () => {
+  const [user, setUser] = useState(null);
+
+  /*controler si l utlisateur est connecté */
+  onAuthStateChanged(auth, (currentUser) => {
+    setUser(currentUser);
+  });
+
   return (
     <Routes>
+      <Route
+        path="/connectmodal"
+        element={user ? <CreatePost></CreatePost> : <ConnectModal />}
+      ></Route>
+
       <Route path="/" element={<Accueil />}></Route>
-      <Route path="/connectmodal" element={<ConnectModal />}></Route>
       <Route path="/login" element={<Login />}></Route>
       <Route path="signup" element={<SignUp />}></Route>
     </Routes>
---
+++ src/components/CreatePost.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const CreatePost = () => {
+  return (
+    <div>
+      <form>
+        <textarea placeholder="Message..."></textarea>
+        <input type="submit" value="Envoyer"></input>
+      </form>
+    </div>
+  );
+};
+
+export default CreatePost;

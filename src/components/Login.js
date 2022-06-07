ec952796449e22077f6b8bc884edcf0beff2a80a
jodev08 <j.stonskadev@gmail.com>
2022-06-07 11:15:02 +0200

    login et signin fonctionne

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,16 +1,31 @@
 import { signInWithEmailAndPassword } from "firebase/auth";
-import React, { useRef } from "react";
+import React, { useRef, useState } from "react";
 import { auth } from "../utils/firebase.config";
 
 const Login = () => {
   const loginEmail = useRef();
   const loginPassword = useRef();
 
+  const handleLogin = async (e) => {
+    e.preventDefault();
+
+    try {
+      const user = await signInWithEmailAndPassword(
+        auth,
+        loginEmail.current.value,
+        loginPassword.current.value
+      );
+      console.log(user);
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
+
   return (
     <div>
       <h3>se connecter</h3>
 
-      <form>
+      <form onSubmit={(e) => handleLogin(e)}>
         <input type="email" placeholder="Email" required ref={loginEmail} />
         <input
           type="password"
